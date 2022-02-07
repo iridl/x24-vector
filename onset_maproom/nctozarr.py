32ba1ea66043c7dcce0b21d7f0d3b34c7c7c0061
remic <remic@iri.columbia.edu>
2022-02-07 14:42:00 -0500

    to keep the record of converting soilgrids to zarr

---
+++ onset_maproom/nctozarr.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pandas as pd
+import xarray as xr
+import datetime as dt
+from pathlib import Path
+import pyaconf
+
+CONFIG = pyaconf.load(os.environ["CONFIG"])
+
+NC_PATH = CONFIG["nc_path"]
+ZARR_PATH = CONFIG["zarr_path"]
+
+DATA_PATH = Path(NC_PATH)
+DATA_FILE = list(sorted(DATA_PATH.glob("*.nc")))
+
+print(DATA_FILE[0])
+
+data = xr.open_dataset(
+  DATA_FILE[0],
+)
+
+data.to_zarr(
+  store = ZARR_PATH
+)
