63ddd399afa6ad71f07ebb7f14c2b094666fc059
Rémi Cousin <remic@iri.columbia.edu>
2022-10-07 14:17:06 -0400

    wrapping up documentation

--- onset_maproom/agronomy.py
+++ onset_maproom/agronomy.py
@@ -9,34 +9,39 @@ def soil_plant_water_balance(
 ):
     """Compute soil-plant-water balance from yesterday to today.
     The balance is defined as:
-    `sm`(t) + `drainage`(t) = `sm`(t-1) + `peffective`(t) - `et`(t)
+    
+    `sm` (t) + `drainage` (t) = `sm` (t-1) + `peffective` (t) - `et` (t)
+    
     where:
+    
     `sm` is the soil moisture and can not exceed total available water `taw`.
+    
     `drainage` is the residual soil moisture occasionally exceeding `taw` that drains through the soil.
+    
     `peffective` is the effective precipitation that enters the soil and is the `rain` minus a `runoff`.
+    
     `et` is the evapotranspiration yielded by the plant.
     
     Parameters
-    ------
+    ----------
     sm_yesterday : DataArray
         soil moisture of yesterday.
     rain : DataArray
         rainfall today.
     et : DataArray
         evapotransipiration of the plant today.
-    taw: DataArray
+    taw : DataArray
         total available water that represents the maximum water capacity of the soil
-    runoff : DataArray
-        amount of rainfall lost to runoff today (default `runoff`=0).
+    runoff : DataArray, optional
+        amount of rainfall lost to runoff today (default `runoff` =0).
+        
     Returns
     -------
     sm, peffective, drainage : Tuple of DataArray
         today soil moisture, effective precipitation and drainage
-    See Also
-    --------
-    Notes
-     -----
+    
     """
+    
     # Water Balance
     peffective = rain - runoff
     wb = sm_yesterday + peffective - et
