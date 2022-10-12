a82cad700e7aeb3fe970cc02b2b6942e9c8c5af0
Rémi Cousin <remic@iri.columbia.edu>
2022-10-12 13:02:28 -0400

    simpler step

--- onset_maproom/agronomy.py
+++ onset_maproom/agronomy.py
@@ -2,10 +2,9 @@ import xarray as xr
 
 def soil_plant_water_balance(
     sm_yesterday,
-    rain,
+    peffective,
     et,
-    taw,
-    runoff=0
+    taw
 ):
     """Compute soil-plant-water balance from yesterday to today.
     The balance is defined as:
@@ -16,9 +15,10 @@ def soil_plant_water_balance(
     
     `sm` is the soil moisture and can not exceed total available water `taw`.
     
-    `drainage` is the residual soil moisture occasionally exceeding `taw` that drains through the soil.
+    `drainage` is the residual soil moisture occasionally exceeding `taw`
+    that drains through the soil.
     
-    `peffective` is the effective precipitation that enters the soil and is the `rain` minus a `runoff`.
+    `peffective` is the effective precipitation that enters the soil.
     
     `et` is the evapotranspiration yielded by the plant.
     
@@ -26,25 +26,22 @@ def soil_plant_water_balance(
     ----------
     sm_yesterday : DataArray
         soil moisture of yesterday.
-    rain : DataArray
-        rainfall today.
+    peffective : DataArray
+        effective precipitation today.
     et : DataArray
         evapotransipiration of the plant today.
     taw : DataArray
-        total available water that represents the maximum water capacity of the soil
-    runoff : DataArray, optional
-        amount of rainfall lost to runoff today (default `runoff` =0).
+        total available water that represents the maximum water capacity of the soil.
         
     Returns
     -------
-    sm, peffective, drainage : Tuple of DataArray
-        today soil moisture, effective precipitation and drainage
+    sm, drainage : Tuple of DataArray
+        today soil moisture and drainage
     
     """
     
     # Water Balance
-    peffective = rain - runoff
     wb = sm_yesterday + peffective - et
     drainage = (wb - taw).clip(min=0)
     sm = wb - drainage
-    return sm, peffective, drainage
+    return sm, drainage
