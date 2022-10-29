kgraaf/python-maprooms-fork

commit c22837f806d376ff331a14035973b031a7bbafba

Don't reimplement functionality that's built into shapely

fbfmaproom/fbfmaproom.py
@@ -874,7 +874,7 @@ def update_selected_region(position, mode, pathname):
     else:
         geom, attrs = retrieve_geometry(country_key, (x, y), mode, None)
         if geom is not None:
-            positions = pingrid.shapely_to_leaflet(geom)
+            positions = shapely.geometry.mapping(geom)["coordinates"]
             key = str(attrs["key"])
     if positions is None:
         positions = ZERO_SHAPE

pingrid.py
@@ -653,45 +653,6 @@ def sel_periodic(ds, dim, vals, period=360.0):
     return ds
 
 
-def mpoly_leaflet_to_shapely(polys):
-    return MultiPolygon(poly_leaflet_to_shapely(poly) for poly in polys)
-
-
-def poly_leaflet_to_shapely(poly):
-    exterior = poly[0]
-    interiors = poly[1:]
-    return Polygon(
-        ring_leaflet_to_shapely(exterior),
-        [ring_leaflet_to_shapely(interior) for interior in interiors],
-    )
-
-
-def ring_leaflet_to_shapely(ring):
-    return [(x, y) for y, x in ring]
-
-
-def shapely_to_leaflet(geom):
-    if isinstance(geom, MultiPolygon):
-        return mpoly_shapely_to_leaflet(geom)
-    elif isinstance(geom, Polygon):
-        return poly_shapely_to_leaflet(geom)
-    else:
-        raise Exception(f"Don't know how to handle {type(geom)}")
-
-
-def mpoly_shapely_to_leaflet(mpoly):
-    return [poly_shapely_to_leaflet(poly) for poly in mpoly.geoms]
-
-
-def poly_shapely_to_leaflet(poly):
-    return [ring_shapely_to_leaflet(poly.exterior)] + [
-        ring_shapely_to_leaflet(interior) for interior in poly.interiors
-    ]
-
-
-def ring_shapely_to_leaflet(ring):
-    return [(y, x) for x, y in ring.coords]
-
 
 # Flask utils
 

iridl/x24-vector

commit 0096ca71da09acd739cf984a90468760c569b36b

Use dlf.GeoJSON instead of dlf.Polygon
Fixes a bug introduced in c22837f806d376ff331a14035973b031a7bbafba

I thought leaflet represented polygons/multipolygons the same way
GeoJSON does, but it doesn't. The outermost structure is the same, but
each individual point is represented as (lon, lat) in GeoJSON
but (lat, lon) in leaflet.

fbfmaproom/fbflayout.py
@@ -96,13 +96,14 @@ def map_layout():
             ),
             dlf.LayerGroup(
                 [
-                    dlf.Polygon(
-                        positions=[(0, 0), (0, 0)],
-                        color="rgb(49, 109, 150)",
-                        fillColor="orange",
-                        fillOpacity=0.1,
-                        weight=2,
-                        id="feature",
+                    dlf.GeoJSON(
+                        options=dict(
+                            color="rgb(49, 109, 150)",
+                            fillColor="orange",
+                            fillOpacity=0.1,
+                            weight=2,
+                        ),
+                        id="outline",
                     ),
                     dlf.Marker(
                         [

fbfmaproom/fbfmaproom.py
@@ -852,7 +852,7 @@ def map_click(pathname, lat_lng):
 
 
 @APP.callback(
-    Output("feature", "positions"),
+    Output("outline", "data"),
     Output("geom_key", "data"),
     Input("marker", "position"),
     Input("mode", "value"),
@@ -862,7 +862,7 @@ def update_selected_region(position, mode, pathname):
     country_key = country(pathname)
     y, x = position
     c = CONFIG["countries"][country_key]
-    positions = None
+    selected_shape = None
     key = None
     if mode == "pixel":
         (x0, y0), (x1, y1) = calculate_bounds(
@@ -871,17 +871,18 @@ def update_selected_region(position, mode, pathname):
         pixel = box(x0, y0, x1, y1)
         geom, _ = retrieve_geometry(country_key, tuple(c["marker"]), "0", None)
         if pixel.intersects(geom):
-            positions = [[[[y0, x0], [y1, x0], [y1, x1], [y0, x1]]]]
+            selected_shape = box(x0, y0, x1, y1)
         key = str([[y0, x0], [y1, x1]])
     else:
         geom, attrs = retrieve_geometry(country_key, (x, y), mode, None)
         if geom is not None:
-            positions = shapely.geometry.mapping(geom)["coordinates"]
+            selected_shape = geom
             key = str(attrs["key"])
-    if positions is None:
-        positions = ZERO_SHAPE
+    if selected_shape is None:
+        selected_shape = ZERO_SHAPE
 
-    return positions, key
+    geojson = shapely.geometry.mapping(selected_shape)
+    return {'features': [geojson]}, key
 
 
 def box(x0, y0, x1, y1):

fbfmaproom/tests/test_fbfmaproom.py
@@ -424,30 +424,44 @@ def test_stats():
         assert resp.status_code == 200
 
 def test_update_selected_region_pixel():
-    positions, key = fbfmaproom.update_selected_region.__wrapped__(
+    feature_collection, key = fbfmaproom.update_selected_region.__wrapped__(
         [6.875, 43.875],
         'pixel',
         '/fbfmaproom/ethiopia',
     )
-    assert positions == [[[[6.75, 43.75], [7.0, 43.75], [7.0, 44.0], [6.75, 44.0]]]]
+    expected = {
+        'features': [
+            {
+                'type': 'Polygon',
+                'coordinates': ((
+                    (43.75, 6.75),
+                    (44.0, 6.75),
+                    (44.0, 7.0),
+                    (43.75, 7.0),
+                    (43.75, 6.75),
+                ),)
+            }
+        ]
+    }
+    assert feature_collection == expected
     assert key == "[[6.75, 43.75], [7.0, 44.0]]"
 
 def test_update_selected_region_level0():
-    positions, key = fbfmaproom.update_selected_region.__wrapped__(
+    feature_collection, key = fbfmaproom.update_selected_region.__wrapped__(
         [6.875, 43.875],
         '0',
         '/fbfmaproom/ethiopia',
     )
-    assert len(positions[0][0]) == 1323
+    assert len(feature_collection['features'][0]['coordinates'][0][0]) == 1323
     assert key == "ET05"
 
 def test_update_selected_region_level1():
-    positions, key = fbfmaproom.update_selected_region.__wrapped__(
+    feature_collection, key = fbfmaproom.update_selected_region.__wrapped__(
         [6.875, 43.875],
         '1',
         '/fbfmaproom/ethiopia',
     )
-    assert len(positions[0][0]) == 143
+    assert len(feature_collection['features'][0]['coordinates'][0][0]) == 143
     assert key == "(ET05,ET0505)"
 
 def test_update_popup_pixel():