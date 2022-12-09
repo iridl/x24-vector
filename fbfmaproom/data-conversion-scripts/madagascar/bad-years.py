08a8142354a1c35cba5ef7ed5dd3fa797687317b
Kevin Graaf <82181542+kgraaf@users.noreply.github.com>
2022-12-09 14:56:15 -0500

    added second bad years

--- fbfmaproom/data-conversion-scripts/madagascar/bad-years.py
+++ fbfmaproom/data-conversion-scripts/madagascar/bad-years.py
@@ -1,9 +1,16 @@
 import cftime
 import pandas as pd
 
-df = pd.read_csv('MadaBadYear4Tool0622Workshp - MadaBadYear4Tool0622Workshp.csv')
-df = df.set_index(df['Year'].apply(lambda x: cftime.Datetime360Day(x, 11, 16)).rename('T'))
+bad = pd.read_csv('MadaBadYear4Tool0622Workshp - MadaBadYear4Tool0622Workshp.csv')
+
+df = bad.set_index(bad['Year'].apply(lambda x: cftime.Datetime360Day(x, 11, 16)).rename('T'))
 df = df.drop('Year', axis='columns')
 df = df.rename({'Rank': 'rank'}, axis='columns')
 ds = df.to_xarray()
-ds.to_zarr('/home/aaron/scratch/iri/data/aaron/fbf-candidate/madagascar/bad-years-ond.zarr')
+ds.to_zarr('bad-years-ond.zarr')
+
+df = bad.set_index(bad['Year'].apply(lambda x: cftime.Datetime360Day(x, 1, 16)).rename('T'))
+df = df.drop('Year', axis='columns')
+df = df.rename({'Rank': 'rank'}, axis='columns')
+ds = df.to_xarray()
+ds.to_zarr('bad-years-djf.zarr')
