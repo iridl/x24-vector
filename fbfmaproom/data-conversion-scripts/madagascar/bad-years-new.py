edb36b7ff124edfc16132b3ebf024b3e271a778a
Kevin Graaf <82181542+kgraaf@users.noreply.github.com>
2022-12-12 19:19:32 -0500

    added new bad years

---
+++ fbfmaproom/data-conversion-scripts/madagascar/bad-years-new.py
@@ -0,0 +1,13 @@
+import cftime
+import datetime
+import pandas as pd
+import xarray as xr
+
+bad = pd.read_csv('MADBadYear4ToolDec2022Workshop.csv')
+
+years = [ cftime.Datetime360Day(y + 1, 1, 16) for y in bad.iloc[:,0].to_list() ]
+ranks = bad.iloc[:,1].to_list()
+
+ds = xr.Dataset(data_vars={"rank": xr.DataArray(ranks, coords={"T": years})})
+
+ds.to_zarr('bad-years-new.zarr')
