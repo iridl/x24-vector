127524c86c8a1a2dac34a690c688cd822e1e1c6f
remic <remic@iri.columbia.edu>
2022-12-14 09:12:12 -0500

    cleaning up documentation

--- components.py
+++ components.py
@@ -13,20 +13,17 @@ def Number(id, default, min=0, max=5):
     ----------
     id : str
         ID used for Dash callbacks.
-    default : int
+    default : float
         Default value that is displayed in the input box when user loads the page.
-    min : int
-        Minimum value the user can select from.
-    max : int
-        Maximum value the user can select from.
+    min : float, optional
+        Minimum value the user can select from. Default is 0.
+    max : float, optional
+        Maximum value the user can select from. Defautl is 5.
+
     Returns
     -------
     dbc.Input : component
         dbc Input component with numerical inputs.
-    Notes
-    -----
-    Examples
-    --------
     """
     return [dbc.Input(id=id, type="number", min=min, max=max, size="sm",
                      className="m-1 d-inline-block w-auto", debounce=True, value=str(default))]
@@ -41,8 +38,10 @@ def Month(id, default):
     ----------
     id : str
         ID used for Dash callbacks.
-    default : int
-        Default value that is displayed in the input box when user loads the page.
+    default : str
+        Default month value that is displayed in the input box when user loads the page.
+        Valid values are the first 3 letters of the month in English, lower caps.
+
     Returns
     -------
     dbc.Select : component
@@ -78,6 +77,8 @@ def DateNoYear(id, defaultDay, defaultMonth):
         Default value that is displayed in the input box when user loads the page.
     defaultMonth : str
         Default value that is displayed in the dropdown when user loads the page.
+        Valid values are first 3 letters of the month with inital in upper caps.
+
     Returns
     -------
     [dbc.Input, dbc.Select] : list
@@ -114,11 +115,13 @@ def Sentence(*elems):
     ----------
     elems : list
         A list of elements to be included in constructing the full sentence.
+
     Returns
     -------
     dbc.Form : component
         A dbc Form which formats all list elements into a sentence
         where the user can interact with Inputs within the sentence.
+
     Notes
     ------
     Still in development.
@@ -155,13 +158,16 @@ def Block(title, *body, ison=True, width="100%"): #width of the block in its con
     body : str, dbc
        Any number of elements which can be of various types to be
        formatted as a sentence within the card body.
-    width : str
+    ison : boolean, optional
+       Card is not displayed if False, default is True
+    width : str, optional
         html style attribute value to determine width of the card within its parent container.
+        Default `width` ="100%".
+
     Returns
     -------
     dbc.Card : component
        A dbc Card which has a pre-formatted title and body where the body can be any number of elements.
-       Default ` width='100%'`.
     """
     if ison:
         the_display = "inline-block"
@@ -177,6 +183,7 @@ def Options(options,labels=None):
 
     Creates a dictionary of 'labels' and 'values'
     to be used as options for an element within different Dash components.
+
     Parameters
     ----------
     options : list
@@ -184,11 +191,13 @@ def Options(options,labels=None):
     labels : list
         List of values (str, int, float, etc.) which are labels representing the data values defined in `options`,
         which do not have to be identical to the values in `options`.
+
     Returns
     -------
     list of dicts
         A list which holds a dictionary for each `options` value where key 'value' == `options` value,
         and key 'label' == `labels` value if `label` != 'None'.
+
     Notes
     -----
         The default `labels=None` will use `options` to define both the labels and the values.
@@ -212,25 +221,28 @@ def Select(id, options, labels=None, init=0):
     """Provides a selector for a list of options.
 
     Creates a auto-populated dash bootstrap components Select component.
+
     Parameters
     ----------
     id : str
         ID used for Dash callbacks.
     options : list
         List of values (str, int, float, etc.) which are the options of values to select from some data.
-    labels : list
+    labels : list, optional
         List of values (str, int, float, etc.) which are labels representing the data values defined in `options`,
-        which do not have to be identical to the values in `options`.
-    init : int
+        which do not have to be identical to the values in `options` , which are the default.
+    init : int, optional
         Index value which determines which value from the list of `options` will be displayed when user loads page.
+        Default is 0.
+
     Returns
     -------
     dbc.Select : component
         A dbc dropdown which is auto-populated with 'values' and 'labels' where key 'value' == `options` value,
         and key 'label' == `labels` value if `label` != 'None'.
+
     Notes
     -----
-        The default `labels=None` will use `options` to define both the labels and the values.
         If `labels` is populated with a list, the labels can be different from the data values.
         In this case, the values must still match the actual data values, whereas the labels do not.
         An error will be thrown if the number of elements in `options` list != `labels` list.
