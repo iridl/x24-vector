cfc6438a00dbcb3807ee4d2393cb3b543570b145
Drew Resnick <drewr@iri.columbia.edu>
2023-01-24 17:51:59 -0500

    remove extra set_up_dims func

--- enacts/enactstozarr.py
+++ enacts/enactstozarr.py
@@ -62,16 +62,6 @@ xr.Dataset().merge(rr_mrg).to_zarr(
 if not os.access(CONFIG["data_dir"], os.W_OK | os.X_OK):
     sys.exit("can't write to output directory")
 
-def set_up_dims(xda):
-    datestr = Path(xda.encoding["source"]).name.split("_")[2]
-    year = int(datestr[0:4])
-    month = int(datestr[4:6])
-    dekad = int(datestr[6:7])
-    day = (dekad - 1) * 10 + 1
-    xda = xda.expand_dims(T = [dt.datetime(year, month, day)])
-    xda = xda.rename({'Lon': 'X','Lat': 'Y'})
-    return xda
-
 def convert(variable):
     netcdf = list(sorted(Path(CONFIG['data_src'][variable]).glob("*.nc")))
 
