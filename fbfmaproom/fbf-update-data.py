a3346a07fbef78c1d7ef9eb63815745bae506285
Aaron Kaplan <aaron@iri.columbia.edu>
2023-04-20 09:02:53 -0400

    Add Lesotho CHIRPS

--- fbfmaproom/fbf-update-data.py
+++ fbfmaproom/fbf-update-data.py
@@ -314,12 +314,20 @@ url_datasets = [
         "https://iridl.ldeo.columbia.edu/home/.audreyv/.dle_lms/.Lesotho/.ENACTS/.ALL/.monthly/.rainfall/.rfe/T/(Dec-Feb)/seasonalAverage/3/mul/",
     ),
     (
+        "lesotho/chirps-djf",
+        "http://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.monthly/.global/.precipitation/X/26.925/29.5875/RANGE/Y/-30.7875/-28.425/RANGE/T/(Dec-Feb)/seasonalAverage/c%3A/3//units//months/def/%3Ac/mul//name//precipitation/def/",
+    ),
+    (
         "lesotho/pnep-ond",
         "http://iridl.ldeo.columbia.edu/home/.remic/.Lesotho/.Forecasts/.NextGen/.OND_PRCPPRCP_CCAFCST/.NextGen/.FbF/.pne/",
     ),
     (
         "lesotho/enacts-precip-ond",
         "https://iridl.ldeo.columbia.edu/home/.audreyv/.dle_lms/.Lesotho/.ENACTS/.ALL/.monthly/.rainfall/.rfe/T/(Oct-Dec)/seasonalAverage/3/mul/",
+    ),
+    (
+        "lesotho/chirps-ond",
+        "http://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.monthly/.global/.precipitation/X/26.925/29.5875/RANGE/Y/-30.7875/-28.425/RANGE/T/(Oct-Dec)/seasonalAverage/c%3A/3//units//months/def/%3Ac/mul//name//precipitation/def/",
     ),
 ]
 
