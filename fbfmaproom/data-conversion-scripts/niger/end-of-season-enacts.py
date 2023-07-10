26e9f103ab7da0109bd5c93ad372449f3b522b8e
Aaron Kaplan <aaron@iri.columbia.edu>
2023-07-10 14:40:57 -0400

    Niger end of season from ENACTS

---
+++ fbfmaproom/data-conversion-scripts/niger/end-of-season-enacts.py
@@ -0,0 +1,9 @@
+import cftime
+import xarray as xr
+
+ds = xr.open_dataset('enacts_cos.nc')
+ds = ds.rename(Time='time')
+ds['time'] = [cftime.Datetime360Day(x, 8, 16) for x in ds['time']]
+ds = ds.sel(time=ds.time > cftime.Datetime360Day(1991, 1, 1))
+print(ds)
+ds.to_zarr('niger-end-of-season-enacts.zarr')
