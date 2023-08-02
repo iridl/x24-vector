d8b339d711347391fcde089f8483625f84ff3f70
Aaron Kaplan <aaron@iri.columbia.edu>
2023-08-02 11:52:21 -0400

    Put latitude in sorted order for end of season datasets

--- fbfmaproom/data-conversion-scripts/niger/end-of-season-enacts.py
+++ fbfmaproom/data-conversion-scripts/niger/end-of-season-enacts.py
@@ -5,5 +5,6 @@ ds = xr.open_dataset('enacts_cos.nc')
 ds = ds.rename(Time='time')
 ds['time'] = [cftime.Datetime360Day(x, 8, 16) for x in ds['time']]
 ds = ds.sel(time=ds.time > cftime.Datetime360Day(1991, 1, 1))
+ds = ds.sortby('latitude')
 print(ds)
 ds.to_zarr('niger-end-of-season-enacts.zarr')
--- fbfmaproom/data-conversion-scripts/niger/end-of-season.py
+++ fbfmaproom/data-conversion-scripts/niger/end-of-season.py
@@ -3,5 +3,6 @@ import xarray as xr
 
 ds = xr.open_dataset('cos.nc')
 ds['time'] = [cftime.Datetime360Day(x, 8, 16) for x in range(1991, 2023)]
+ds = ds.sortby('latitude')
 print(ds)
 ds.to_zarr('niger-end-of-season.zarr')
