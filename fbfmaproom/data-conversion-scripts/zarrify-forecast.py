dbf5b3d1baeaecb20158430f046c0069d6c6544c
Aaron Kaplan <aaron@iri.columbia.edu>
2024-03-03 21:47:53 -0500

    Work around bug in pycpt that puts T 12 hours too early
    Only showed up now because this is the first time we're using a season
    with an even number of months.

--- fbfmaproom/data-conversion-scripts/zarrify-forecast.py
+++ fbfmaproom/data-conversion-scripts/zarrify-forecast.py
@@ -73,17 +73,15 @@ def convert_ds(ds, issue_month):
 def read_v2_one_issue_month(path):
     hindcasts = xr.Dataset(
         dict(
-            mu= xr.open_dataarray(path / 'MME_deterministic_hindcasts.nc'),
-            var=xr.open_dataarray(path / 'MME_hindcast_prediction_error_variance.nc'),
+            mu=open_and_fix(path / 'MME_deterministic_hindcasts.nc'),
+            var=open_and_fix(path / 'MME_hindcast_prediction_error_variance.nc'),
         ),
     )
     mu_files = list(path.glob('MME_deterministic_forecast_*.nc'))
     var_files = list(path.glob('MME_forecast_prediction_error_variance_*.nc'))
-    print(mu_files)
-    print(bool(mu_files))
     if mu_files and var_files:
-        mu_da = xr.open_mfdataset(mu_files).load().data_vars.values().__iter__().__next__()
-        var_da = xr.open_mfdataset(var_files).load().data_vars.values().__iter__().__next__()
+        mu_da = open_and_fix_mf(mu_files).load()
+        var_da = open_and_fix_mf(var_files).load()
         forecasts = xr.Dataset(
             dict(mu=mu_da, var=var_da)
         )
@@ -120,16 +118,18 @@ def to_360_coord(c):
     ]
                 
 def to_360_ds(ds):
-    # Just dropping Ti and Tf for now. If we need them we can convert them.
-    ds = ds.drop_vars(['Ti', 'Tf'])
     ds['T'] = to_360_coord(ds['T'])
+    ds['S'] = to_360_coord(ds['S'])
     return ds
 
 def load_pne(path):
     if (path / 'obs.nc').is_file():
-        obs_da = xr.open_dataarray(path / 'obs.nc')
+        obs_da = open_and_fix(path / 'obs.nc')
     else:
+        # for backwards compatibility with some old forecast datasets,
+        # try again with tsv
         obs_da = next(iter(cptio.open_cptdataset(path / 'obs.tsv').data_vars.values()))
+        obs_da = fix_t(obs_da)
     obs = xr.Dataset(dict(
         obs=obs_da
     ))
@@ -142,6 +142,26 @@ def load_pne(path):
             pne = convert_ds(pne, monthno).swap_dims(T='S')
             pne_per_issue_month.append(pne)
     return xr.merge(pne_per_issue_month, compat='no_conflicts')
+
+
+def open_and_fix(path):
+    da = xr.open_dataarray(path)
+    da = fix_t(da)
+    return da
+
+def open_and_fix_mf(paths):
+    da = next(iter(xr.open_mfdataset(paths).data_vars.values()))
+    da = fix_t(da)
+    return da
+
+
+def fix_t(ds):
+    '''Work around a bug in cptio that puts Tf 24 hours too early, and consequently T 12 hours too early.'''
+    if ds['Tf'][0].dt.day.values != 1:
+        ds['T'] = ds['T'] + np.timedelta64(12, 'h')
+    # Just dropping Ti and Tf for now. If we need them we can convert them.
+    ds = ds.drop_vars(['Ti', 'Tf'])
+    return ds
 
 
 def zarrify(path, datadir):
