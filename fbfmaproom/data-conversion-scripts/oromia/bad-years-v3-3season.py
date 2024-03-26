55271451e5249adbdad46f9259102853df974d88
Xandre Chourio <xchourio@shortfin.iri.columbia.edu>
2024-03-26 18:45:04 -0400

    adjustments fbfmaproom-sample.yaml file and bad-years production script.

--- fbfmaproom/data-conversion-scripts/oromia/bad-years-v3-3season.py
+++ fbfmaproom/data-conversion-scripts/oromia/bad-years-v3-3season.py
@@ -5,24 +5,39 @@ import xarray as xr
 import sys
 import os
 
+
 dirout='/data/aaron/fbf-candidate/'
 regionsdic = {
-        "oromia": {"file": "/data/aaron/fbf-candidate/original-data/oromia/bad_years.csv"}
+    "ethiopia/southern-oromia": {"file": "/data/aaron/fbf-candidate/original-data/oromia/bad_years.csv"},
+  "djibouti": {"file": "/data/aaron/fbf-candidate/original-data/djibouti/bad_years.csv"}
+    
 }
 
-seasons=["DJF","JFM","FMA","MAM","AMJ","MJJ","JJA","JAS","ASO","SON","OND","NDJ"]
+
+seasons = {"djf":1, 
+           "jfm":2, 
+           "fma":3, 
+           "mam":4, 
+           "amj":5, 
+           "mjj":6, 
+           "jja":7, 
+           "jas":8, 
+           "aso":9, 
+           "son":10, 
+           "ond":11, 
+           "ndj":12}
 
 if  len(sys.argv) < 3:
     print("\033[91m"+"Two arguments is expected. (season, region)"+"\033[0m")
-    print("\033[1m"+"Example: python bad-years-v3-3season.py ond oromia"+"\033[0m")
+    print("\033[1m"+"Example: python bad-years-v3-3season.py ond djibouti"+"\033[0m")
     sys.exit()
-elif not sys.argv[1].upper() in seasons: 
-    print("\033[91m"+"The station is not allowed."+"\033[0m")
-    print("\033[1m"+"Example: "+", ".join(list(map(str.lower,seasons))) +"\033[0m")
+elif not sys.argv[1] in seasons: 
+    print("\033[91m"+"The season is not allowed."+"\033[0m")
+    print("\033[1m"+"Example: "+", ".join(seasons.keys()) +"\033[0m")
     sys.exit()
 elif not sys.argv[2].lower() in regionsdic: 
     print("\033[91m"+"The region is not defined."+"\033[0m")
-    print("\033[1m"+"regions avalibale: "+ (", ".join(regionsdic.keys())) +"\033[0m")
+    print("\033[1m"+"regions available: "+ (", ".join(regionsdic.keys())) +"\033[0m")
     sys.exit()
 else:
     season = sys.argv[1].lower()
@@ -30,13 +45,12 @@ else:
     
 
 bad=pd.read_csv(regionsdic[region]['file'], skiprows=1)
-years = [cftime.Datetime360Day(y + 1, 1, 16) for y in bad['year'].tolist()]
+years = [cftime.Datetime360Day(y, seasons[season], 16) for y in bad['year'].tolist()]
 if season in bad.columns:
     ranks = bad[season].to_list()
 else:
-    print("\033[91m"+"The station "+season+" is not available in the file "+regionsdic[region]['file']+"."+"\033[0m")
+    print("\033[91m"+"The season "+season+" is not available in the file "+regionsdic[region]['file']+"."+"\033[0m")
     sys.exit()
-
 
 ds = xr.Dataset(data_vars={"rank": xr.DataArray(ranks, coords={"T": years})})
 
