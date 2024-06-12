78fb7bf2065621b23e2c6d66e7756efb917ac3c8
Sheen Zutshi <sz3116@storm.iri.columbia.edu>
2024-06-12 09:53:37 -0400

    Changed dimensions to all be hurs ex(X,Y,T)

--- pepsico/comparison.py
+++ pepsico/comparison.py
@@ -1,16 +1,16 @@
 import xarray as xr
 import os
-#from pathlib import Path 
 
-# defining the path to file
+
 ds_zarr = '/data/remic/mydatafiles/zarr/test/hurs'
+ds_nc_dir = '/Data/data24/ISIMIP3b/InputData/climate/atmosphere/bias-adjusted/global/daily/historical/MPI-ESM1-2-HR/'
 
-ds_nc_dir = '/Data/data24/ISIMIP3b/InputData/climate/atmosphere/bias-adjusted/global/daily/historical/MPI-ESM1-2-HR/' #mpi-esm1-2-hr_r1i1p1f1_w5e5_historical_hurs_global_daily_1951_1960.nc'
-#iterating over all the years
 
 #only using NetCDF files that contain 'hurs' variable
 def contains_hurs(filename):
     return 'hurs' in filename
+
+
 
 #function that compares nc files(hurs) vs zarred file
 def compare_datasets(nc_file_path, ds_zarr):
@@ -21,17 +21,19 @@ def compare_datasets(nc_file_path, ds_zarr):
     nc_dSet = xr.open_dataset(nc_file_path)[['hurs']]
 
     #mapping from zar to nc dimensions (X,Y,T) vs (lon,lat,time)
-    coord_map = {'X': 'lon', 'Y': 'lat', 'T': 'time'}
+    coord_map = {'lon': 'X', 'lat': 'Y', 'time': 'T'}
 
-    #xararay rename function to reset zarr dimensions to nc labels 
-    zarr_dSet = zarr_dSet.rename({orig_dim: new_dim for orig_dim, new_dim in coord_map.items() if orig_dim in zarr_dSet.dims})
-    zarr_dSet = zarr_dSet.rename({orig_coord: new_coord for orig_coord, new_coord in coord_map.items() if orig_coord in zarr_dSet.coords})
+    #xararay rename function to reset nc labels to zarr
+    nc_dSet = nc_dSet.rename({orig_dim: new_dim for orig_dim, new_dim in coord_map.items() if orig_dim in nc_dSet.dims})
+    nc_dSet = nc_dSet.rename({orig_coord: new_coord for orig_coord, new_coord in coord_map.items() if orig_coord in nc_dSet.coords})
+
+    
 
     #align the zarr chunk with overlapping nc data
-    time_start = max(nc_dSet['time'].min().values, zarr_dSet['time'].min().values) #start time = compare earliest time of both files and choose latest between the 2
-    time_end = min(nc_dSet['time'].max().values, zarr_dSet['time'].max().values) #minimimum of the 2 files max time
-    nc_dSet = nc_dSet.sel(time=slice(time_start, time_end)) #slicing to overlapping part in nc file
-    zarr_dSet = zarr_dSet.sel(time=slice(time_start, time_end)) #overlapping section for zarr file
+    time_start = max(zarr_dSet['T'].min().values, nc_dSet['T'].min().values) #start time = compare earliest time of both files and choose latest between the 2
+    time_end = min(zarr_dSet['T'].max().values, nc_dSet['T'].max().values) #minimimum of the 2 files max time
+    nc_dSet = nc_dSet.sel(T=slice(time_start, time_end)) #slicing to overlapping part in nc file
+    zarr_dSet = zarr_dSet.sel(T=slice(time_start, time_end)) #overlapping section for zarr file
 
 
     #renaming zarr variables to match nc
