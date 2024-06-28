700cbbbb55e853c29c5d239dc1ff29ff966a0115
Sheen Zutshi <sz3116@storm.iri.columbia.edu>
2024-06-28 11:55:38 -0400

    Added compute and write to csv functions, pr conversion, removed unecessary code

---
+++ pepsico/monthly_seasonal_conv_mapping.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import plotly.express as px
+
+def compute_annual_monthly_avg(ds, variable, start_year=None, end_year=None):
+    #compute the avg (variable) for each month across the specified years
+    
+    #if precipitation variable, change from kg to mm per day
+    if variable == 'pr':
+        ds[variable] *= 86400
+    
+    if start_year and end_year:
+        ds = ds.sel(T=slice(f"{start_year}-01-01", f"{end_year}-12-31"))
+
+    # Convert daily data to monthly
+    monthly_ds = ds.resample(T="1M").mean()
+
+    # Compute the average tasmin for each month across all years
+    monthly_avg = monthly_ds.mean(dim=["X", "Y"])
+    annual_monthly_avg = monthly_avg.groupby("T.month").mean(dim="T")
+    df = annual_monthly_avg.to_dataframe().reset_index()
+    return df
+
+def write_to_csv(df, scenario, model, variable, output_dir):
+
+    # Write to CSV file
+    file_path = f'{output_dir}/{scenario}_{model}_{variable}_annual_monthly_avg.csv'
+    df.to_csv(file_path, index=False)
+
+def main(scenario, model, variable, start_year=None, end_year=None, output_dir='/home/sz3116/python-maprooms/pepsico/resources'):
+    #main to run functions
+
+    # Open zarr file 
+    ds = xr.open_zarr(f'//Data/data24/ISIMIP3b/InputData/climate/atmosphere/bias-adjusted/global/daily/historical/{model}/zarr/{variable}')
+
+    print("\nVariable attributes:")
+    for var in ds.data_vars:
+        print(f"\nAttributes for variable '{var}':")
+        print(ds[var].attrs)
+    
+    # Compute the average variable for each month across all years
+    annual_monthly_avg = compute_annual_monthly_avg(ds, variable, start_year, end_year)
+    
+    # Write the data to a CSV file
+    write_to_csv(annual_monthly_avg, scenario, model, variable, output_dir)
+
+    #also print in terminal
+    print(annual_monthly_avg)
+    
+# testing
+if __name__ == "__main__":
+    scenario = "historical"
+    model = "GFDL-ESM4"
+    variable = "pr"
+    start_year = 1950
+    end_year = 2014
+    output_dir = '/home/sz3116/python-maprooms/pepsico/resources'
+
+    main(scenario, model, variable, start_year, end_year, output_dir)
+
+print("done")
