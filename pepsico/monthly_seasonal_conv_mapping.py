fd1f2c7157709875adbc2c37bdc0dbb52cea9b22
Sheen Zutshi <sz3116@storm.iri.columbia.edu>
2024-07-02 11:46:36 -0400

    Adjusted so unit conversion appeared on maps

--- pepsico/monthly_seasonal_conv_mapping.py
+++ pepsico/monthly_seasonal_conv_mapping.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import plotly.express as px
+from plotly.io import write_html
 from dash import Dash, dcc, html, Input, Output
 from urllib.request import urlopen
 import json
@@ -12,8 +13,10 @@ def compute_annual_monthly_avg(ds, variable, start_year=None, end_year=None):
     #if precipitation variable, change from kg to mm per day
     if variable == 'pr':
         ds[variable] *= 86400
+        ds[variable].attrs['units'] = 'mm/day' #rename unit to converted
     elif variable in ['tas', 'tasmin', 'tasmax']:
-        ds[variable] -= 273.15
+        ds[variable] -= 273.15 
+        ds[variable].attrs['units'] = 'Celsius'
 
     if start_year and end_year:
         ds = ds.sel(T=slice(f"{start_year}-01-01", f"{end_year}-12-31"))
@@ -46,7 +49,7 @@ def compute_annual_seasonal_avg(monthly_ds, variable, start_year=None, end_year=
                                    'JJA', 'JAS', 'ASO', 'SON', 'OND', 'NDJ']
 
     return seasonal_avgs_df
-    
+
 
 def write_to_csv(df, scenario, model, variable, output_dir):
 
@@ -60,10 +63,6 @@ def map_averages(ds, variable, start_year=None, end_year=None):
     #different than compute_annual_monthly_avg because keeps the spatial component when averaging
     #this will be inputted into the plotting function
     
-    # If precipitation variable, change from kg to mm per day
-    if variable == 'pr':
-        ds[variable] *= 86400
-    
     if start_year and end_year:
         ds = ds.sel(T=slice(f"{start_year}-01-01", f"{end_year}-12-31"))
 
@@ -73,22 +72,36 @@ def map_averages(ds, variable, start_year=None, end_year=None):
     # Compute the average for each month across all years
     monthly_avg = monthly_ds.groupby("T.month").mean(dim="T")
 
-    return monthly_avg
+    #if precipitation variable, change from kg to mm per day
+    if variable == 'pr':
+        monthly_avg[variable] *= 86400
+        monthly_avg[variable].attrs['units'] = 'mm/day' #rename unit to converted
+    elif variable in ['tas', 'tasmin', 'tasmax']:
+        monthly_avg[variable] -= 273.15 
+        monthly_avg[variable].attrs['units'] = 'Celsius'
 
-def plot_monthly(ds, variable):
+
+def plot_monthly(ds, variable, output_dir):
+    #convert units if needed
+    units = ds[variable].attrs.get('units', 'unknown')
+    
+    
     #print all 12 months (12 different maps)
     for month in range(1, 13):
         monthly_data = ds.sel(month=month)
         fig = px.imshow(
             monthly_data[variable],
-            labels={'color': variable},
-            title=f'{variable} Monthly Average - Month {month}',
+            labels={'color': f'{variable} ({units})'},
+            title=f'Monthly Average ({variable})  - Month {month}',
             origin='lower'
         )
-        fig.show()
+        #fig.show() print automatically
+        output_file = f"{output_dir}/{variable}_monthly_avg_map_{month}.html"
+        write_html(fig, file=output_file)
+        print("Saved to output directory")
+    
 
-
-def main(scenario, model, variable, start_year=None, end_year=None, output_dir='/home/sz3116/python-maprooms/pepsico/resources'):
+def main(scenario, model, variable, start_year=None, end_year=None, output_dir='/home/sz3116/outputs'):
     #main to run functions
 
     # Open zarr file 
@@ -99,6 +112,7 @@ def main(scenario, model, variable, start_year=None, end_year=None, output_dir='
         print(f"\nAttributes for variable '{var}':")
         print(ds[var].attrs)
     
+    
     # Compute the average variable for each month across all years
     annual_monthly_avg = compute_annual_monthly_avg(ds, variable, start_year, end_year)
     
@@ -107,16 +121,18 @@ def main(scenario, model, variable, start_year=None, end_year=None, output_dir='
     
     # Write the data to a CSV file
     write_to_csv(annual_monthly_avg, scenario, model, variable, output_dir)
-    write_to_csv(rolling_seasonal_avg, scenario, model, f'{variable}_rolling', output_dir)
+    write_to_csv(rolling_seasonal_avg, scenario, model, f'{variable}_seasonal', output_dir)
     
+    #Get monthly avgs for mapping
     plotting_monthly = map_averages(ds, variable, start_year, end_year)
-    # Generate map
-    plot_monthly(plotting_monthly, variable)
+    #Generate map
+    plot_monthly(plotting_monthly, variable, output_dir)
     
-   
+    
     #also print in terminal
     print(annual_monthly_avg)
     print(rolling_seasonal_avg)
+    
 
     
     
@@ -124,10 +140,10 @@ def main(scenario, model, variable, start_year=None, end_year=None, output_dir='
 if __name__ == "__main__":
     scenario = "historical"
     model = "GFDL-ESM4"
-    variable = "pr"
+    variable = "tasmin" #options to input: tas, tasmin, tasmax, pr, rlds
     start_year = 1950
     end_year = 2014
-    output_dir = '/home/sz3116/python-maprooms/pepsico/resources'
+    output_dir = '/home/sz3116/outputs'
 
     main(scenario, model, variable, start_year, end_year, output_dir)
 
