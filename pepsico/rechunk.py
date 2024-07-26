8a9e23b369f5b659fe86e3fa619ca6b827ec0396
remic <remic@iri.columbia.edu>
2024-07-26 16:35:42 -0400

    rechunking for all

--- pepsico/rechunk.py
+++ pepsico/rechunk.py
@@ -7,12 +7,10 @@ COMMON_PATH = f'ISIMIP3b/InputData/climate/atmosphere/bias-adjusted/global'
 INPUT_PATH = TOP_PATH / COMMON_PATH / "monthly"
 OUTPUT_PATH = TOP_PATH / COMMON_PATH / "monthly_rechunked"
 
-#for scenario_path in INPUT_PATH.iterdir():
-for scenario_path in [(INPUT_PATH / "ssp126")]:
-    #for model_path in scenario_path.iterdir():
-    for model_path in [(scenario_path / "GFDL-ESM4")]:
+for scenario_path in INPUT_PATH.iterdir():
+    for model_path in scenario_path.iterdir():
         for var in [
-            #"hurs", "huss", "pr", "prsn", "ps", "rlds", "sfcwind", "tas", "tasmax",
+            "hurs", "huss", "pr", "prsn", "ps", "rlds", "sfcwind", "tas", "tasmax",
             "tasmin",
         ]:
             print(model_path / "zarr" / var)
@@ -22,4 +20,4 @@ for scenario_path in [(INPUT_PATH / "ssp126")]:
             )
             print(output_path)
             output_path.mkdir(parents=True, exist_ok=True)
-            monthly.to_zarr(store=output_path, )
+            monthly.to_zarr(store=output_path)
